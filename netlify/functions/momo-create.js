40acd94da8332fa96ec33805e700a5d5c930f402
hoangphuc206 <hphuccc206@gmail.com>
2025-11-18 16:50:27 +0700

    Update momo-create.js

--- netlify/functions/momo-create.js
+++ netlify/functions/momo-create.js
@@ -7,14 +7,19 @@ const crypto = require('crypto');
 // -----------------------------------------------------------------------------
 // 1. CẤU HÌNH MOMO (Sử dụng biến môi trường là cách TỐT NHẤT)
 // Hiện tại chúng ta dùng giá trị cứng để dễ test, nhưng nên chuyển sang process.env
-// -----------------------------------------------------------------------------
-const accessKey = 'F8BBA842ECF85';
-const secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
-const partnerCode = 'MOMO';
+// --------------------------------------------------------
 
-const redirectUrl = 'https://phucvinhphu.netlify.app/'; // Thay bằng URL trang web của bạn
-const ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b'; 
+// Các giá trị này PHẢI được lấy từ Netlify Environment Variables
+const accessKey = process.env.MOMO_ACCESS_KEY;
+const secretKey = process.env.MOMO_SECRET_KEY;
+const partnerCode = process.env.MOMO_PARTNER_CODE;
 
+// URL trả về và IPN
+const redirectUrl = process.env.MOMO_REDIRECT_URL;
+const ipnUrl = process.env.MOMO_IPN_URL; 
+
+
+//--------------------------
 const host = "test-payment.momo.vn";
 const endpoint = "/v2/gateway/api/create";
 const requestType = "payWithMethod";
@@ -75,7 +80,7 @@ exports.handler = async (event, context) => {
         amount : amount,
         orderId : orderId,
         orderInfo : orderInfo,
-        redirectUrl : redirectUrl,
+       redirectUrl : redirectUrl,
         ipnUrl : ipnUrl,
         lang : lang,
         requestType: requestType,
